d88dd49dcf1bb2436d034cb1e7afc3ea6229679f
Jasen <447124329@qq.com>
2020-03-08 12:40:30 +0800

    save

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ let file = require('./file');
 let unit = require('./unit');
 
 
-console.log('file',file.file())
-console.log('unit',unit)
+setTimeout(()=>{
+  console.log('file',file.file())
+  console.log('unit',unit)
+},1000)
+
+
 
 
 
